21569881482772204dd8aacd9c82df3338da9bd0
ZimmerAllDay <125327151+ZimmerAllDay@users.noreply.github.com>
2023-06-21 15:31:02 -0600

    Update compile_pix.rb
    update to geralds version

--- compile_pix.rb
+++ compile_pix.rb
@@ -1,32 +1,19 @@
-require 'rmagick'
+require 'pixelart'
 
 # Get all the PNG files in the directory
-image_files = Dir.glob("spritesheet_images/*.png").sort
+image_files = Dir.glob("./spritesheet_images/*.png").sort
 
 # Output how many files we found
 puts "Found #{image_files.size} image files"
 
-# Read all the images
-images = image_files.map do |filename|
-  puts "Reading #{filename}..."
-  Magick::Image.read(filename).first
+
+## use a 24 x 24 grid (with sprites / tiles 24x24px each)
+composite = ImageComposite.new( 24, 24, width: 24,
+                                        height: 24)
+
+# add all the images
+image_files.each do |filename|
+   composite << Image.read( filename )
 end
 
-grid_width = 24
-grid_height = 24
-
-# Start with a new image with the size of the entire spritesheet
-composite = Magick::Image.new(24 * grid_width, 24 * grid_height)
-composite.alpha(Magick::ActivateAlphaChannel)  # Enable transparency
-composite.matte_color = 'none'  # Make the whole image transparent
-
-images.each_with_index do |image, index|
-  x = (index % grid_width) * 24
-  y = (index / grid_width) * 24
-  puts "Placing image #{index} at #{x},#{y}"
-  composite = composite.composite(image, x, y, Magick::OverCompositeOp)
-end
-
-output_filename = "spritesheet.png"
-composite.write(output_filename)
-puts "Wrote output to #{output_filename}"
+composite.save( "./spritesheet.png" )
